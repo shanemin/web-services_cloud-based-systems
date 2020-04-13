4d8595aa6b95d1b011a78287d5506664478ff3ea
shanem <shane.m@live.ca>
2020-04-13 15:47:28 +0200

    should only use pip command in venv

--- assignment_2/run_login_service.sh
+++ assignment_2/run_login_service.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export FLASK_APP=login_service.py
 . venv/bin/activate
-pip3 install -r requirements.txt
+pip install -r requirements.txt
 flask run --port=5000
 
--- assignment_2/run_url_service.sh
+++ assignment_2/run_url_service.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export FLASK_APP=url_shortener_service.py
 . venv/bin/activate
-pip3 install -r requirements.txt
+pip install -r requirements.txt
 flask run --port=5001
 
