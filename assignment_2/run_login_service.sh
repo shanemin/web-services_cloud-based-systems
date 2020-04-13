c64037b26f05d8adab72e21a049154accaa0f9ee
shanem <shane.m@live.ca>
2020-04-13 15:35:41 +0200

    updated

--- assignment_2/run_login_service.sh
+++ assignment_2/run_login_service.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export FLASK_APP=login_service.py
-source $(. ) venv/bin/activate
+. venv/bin/activate
 pip3 install -r requirements.txt
 flask run --port=5000
 
--- assignment_2/run_url_service.sh
+++ assignment_2/run_url_service.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export FLASK_APP=url_shortener_service.py
-source $(. )venv/bin/activate
+. venv/bin/activate
 pip3 install -r requirements.txt
 flask run --port=5001
 
